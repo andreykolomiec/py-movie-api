8148a58f1a6df1e02df0f0db2c7431be63af80fb
Andrew <akolomiec982@gmail.com>
2025-03-17 13:00:53 +0200

    Solution2

--- cinema/urls.py
+++ cinema/urls.py
@@ -4,7 +4,7 @@ from cinema.views import movie_detail, movie_list
 
 urlpatterns = [
     path("movies/", movie_list, name="movie_list"),
-    path("movies/<int:pk>", movie_detail, name="movie_detail"),
+    path("movies/<int:pk>/", movie_detail, name="movie_detail"),
 ]
 
 app_name = "cinema"
--- movie/urls.py
+++ movie/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/cinema", include("cinema.urls", namespace="cinema")),
+    path("api/v1/cinema/", include("cinema.urls", namespace="cinema")),
 ]
